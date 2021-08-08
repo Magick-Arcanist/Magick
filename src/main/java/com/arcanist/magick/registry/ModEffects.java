25160a80f30b9ea50ff68b5112b7886ef3e39de9
SoManyBirds <75969918+SoManyBirds@users.noreply.github.com>
2021-08-08 19:00:26 +1000

    testing

--- src/main/java/com/arcanist/magick/registry/ModEffects.java
+++ src/main/java/com/arcanist/magick/registry/ModEffects.java
@@ -1,10 +1,13 @@
 package com.arcanist.magick.registry;
 
 import com.arcanist.magick.statuseffect.effects.*;
+import com.mojang.serialization.Lifecycle;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.MutableRegistry;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 
 public class ModEffects extends StatusEffects{
 
@@ -21,7 +24,11 @@ public class ModEffects extends StatusEffects{
         public static final StatusEffect LOVE = new LoveStatusEffect();
         public static final StatusEffect MANA = new ManaStatusEffect();
 
-        public static void registerEffects() {
+
+
+//this code works in singleplayer but not on servers
+ /*
+   public static void registerEffects() {
                 Registry.register(Registry.STATUS_EFFECT, new Identifier("magick", "fear"), FEAR);
                 Registry.register(Registry.STATUS_EFFECT, new Identifier("magick", "gravity"), GRAVITY);
                 Registry.register(Registry.STATUS_EFFECT, new Identifier("magick", "photosynthesis"), PHOTOSYNTHESIS);
@@ -34,7 +41,24 @@ public class ModEffects extends StatusEffects{
                 Registry.register(Registry.STATUS_EFFECT, new Identifier("magick", "white_light"), WHITE_LIGHT);
                 Registry.register(Registry.STATUS_EFFECT, new Identifier("magick", "love"), LOVE);
                 Registry.register(Registry.STATUS_EFFECT, new Identifier("magick", "mana"), MANA);
-        }
+   }
+*/
+
+    // this works on servers but the textures don't load because it looks for them in minecraft/mob_effect instead of magick/mob_effect
+    public static void registerEffects() {
+        Registry.register(Registry.STATUS_EFFECT,34,  "fear", FEAR);
+        Registry.register(Registry.STATUS_EFFECT, 35, "gravity", GRAVITY);
+        Registry.register(Registry.STATUS_EFFECT, 36, "photosynthesis", PHOTOSYNTHESIS);
+        Registry.register(Registry.STATUS_EFFECT, 37, "spiderclimb", SPIDERCLIMB);
+        Registry.register(Registry.STATUS_EFFECT, 38, "immortal", IMMORTAL);
+        Registry.register(Registry.STATUS_EFFECT,39, "remove_effects", REMOVE_EFFECTS);
+        Registry.register(Registry.STATUS_EFFECT, 40, "ore_sense", ORE_SENSE);
+        Registry.register(Registry.STATUS_EFFECT, 41, "recall", RECALL);
+        Registry.register(Registry.STATUS_EFFECT,42, "red_light", RED_LIGHT);
+        Registry.register(Registry.STATUS_EFFECT, 43, "white_light", WHITE_LIGHT);
+        Registry.register(Registry.STATUS_EFFECT, 44, "love", LOVE);
+        Registry.register(Registry.STATUS_EFFECT, 45, "mana", MANA);
+    }
 
 
 }
